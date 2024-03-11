942c333438ef69a35718ed666d89add84a68b646
btabuenca <bernardo.tabuenca@gmail.com>
2024-03-11 15:47:59 +0100

    Features week 7. Firebase UI and Authentication

--- app/src/main/java/es/upm/btb/helloworldkt/MainActivity.kt
+++ app/src/main/java/es/upm/btb/helloworldkt/MainActivity.kt
@@ -1,7 +1,7 @@
 package es.upm.btb.helloworldkt
 
 import android.Manifest
-import android.content.Context
+import android.app.Activity
 import android.content.pm.PackageManager
 import android.location.Location
 import android.location.LocationListener
@@ -25,6 +25,9 @@ import es.upm.btb.helloworldkt.persistence.room.AppDatabase
 import es.upm.btb.helloworldkt.persistence.room.LocationEntity
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.IdpResponse
+import com.google.firebase.auth.FirebaseAuth
 
 
 class MainActivity : AppCompatActivity(), LocationListener {
@@ -32,7 +35,13 @@ class MainActivity : AppCompatActivity(), LocationListener {
     private lateinit var locationManager: LocationManager
     private var latestLocation: Location? = null
     private val locationPermissionCode = 2
-    lateinit var database: AppDatabase
+    private lateinit var database: AppDatabase
+
+    // companion is the same than an static object in java
+    companion object {
+        private const val RC_SIGN_IN = 123
+    }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -82,7 +91,7 @@ class MainActivity : AppCompatActivity(), LocationListener {
         }
 
         // Location manager init and permisssons
-        locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        locationManager = getSystemService(LOCATION_SERVICE) as LocationManager
         if (ActivityCompat.checkSelfPermission(
                 this,
                 Manifest.permission.ACCESS_FINE_LOCATION
@@ -104,6 +113,10 @@ class MainActivity : AppCompatActivity(), LocationListener {
 
         // Room database init
         database = Room.databaseBuilder(applicationContext, AppDatabase::class.java, "coordinates").build()
+
+
+        // Init authentication flow
+        launchSignInFlow()
     }
 
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
@@ -134,7 +147,7 @@ class MainActivity : AppCompatActivity(), LocationListener {
         }
     }
 
-    override fun onStatusChanged(provider: String?, status: Int, extras: Bundle?) {}
+    override fun onStatusChanged(provider: String?, status: Int, extras: Bundle?) {}    
     override fun onProviderEnabled(provider: String) {}
     override fun onProviderDisabled(provider: String) {}
 
@@ -159,7 +172,7 @@ class MainActivity : AppCompatActivity(), LocationListener {
     }
 
     private fun saveUserIdentifier(userIdentifier: String) {
-        val sharedPreferences = this.getSharedPreferences("AppPreferences", Context.MODE_PRIVATE)
+        val sharedPreferences = this.getSharedPreferences("AppPreferences", MODE_PRIVATE)
         sharedPreferences.edit().apply {
             putString("userIdentifier", userIdentifier)
             apply()
@@ -167,7 +180,7 @@ class MainActivity : AppCompatActivity(), LocationListener {
     }
 
     private fun getUserIdentifier(): String? {
-        val sharedPreferences = this.getSharedPreferences("AppPreferences", Context.MODE_PRIVATE)
+        val sharedPreferences = this.getSharedPreferences("AppPreferences", MODE_PRIVATE)
         return sharedPreferences.getString("userIdentifier", null)
     }
 
@@ -182,8 +195,71 @@ class MainActivity : AppCompatActivity(), LocationListener {
                 startActivity(Intent(this, SettingsActivity::class.java))
                 true
             }
+            R.id.action_logout -> {
+                logout()
+                true
+            }
             else -> super.onOptionsItemSelected(item)
         }
     }
 
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == RC_SIGN_IN) {
+            val response = IdpResponse.fromResultIntent(data)
+            if (resultCode == Activity.RESULT_OK) {
+                // user login succeeded
+                val user = FirebaseAuth.getInstance().currentUser
+                Toast.makeText(this, R.string.signed_in, Toast.LENGTH_SHORT).show();
+                Log.i(TAG, "onActivityResult " + getString(R.string.signed_in));
+
+            } else {
+                // user login failed
+                Log.e(TAG, "Error starting auth session: ${response?.error?.errorCode}")
+                Toast.makeText(this, R.string.signed_cancelled, Toast.LENGTH_SHORT).show();
+                finish()
+            }
+        }
+    }
+
+    private fun launchSignInFlow() {
+        val providers = arrayListOf(
+            AuthUI.IdpConfig.EmailBuilder().build(),
+            AuthUI.IdpConfig.GoogleBuilder().build()
+        )
+
+        startActivityForResult(
+            AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setAvailableProviders(providers)
+                .build(),
+            RC_SIGN_IN
+        )
+    }
+
+    private fun logout() {
+        AuthUI.getInstance()
+            .signOut(this)
+            .addOnCompleteListener {
+                // Restart activity after finishing
+                val intent = Intent(this, MainActivity::class.java)
+                // Clean back stack so that user cannot retake activity after logout
+                intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                startActivity(intent)
+                finish()
+            }
+    }
+
+    private fun updateUIWithUsername() {
+        val user = FirebaseAuth.getInstance().currentUser
+        val userNameTextView: TextView = findViewById(R.id.userNameTextView)
+        user?.let {
+            val name = user.displayName ?: "No Name"
+            userNameTextView.text = "\uD83E\uDD35\u200D♂\uFE0F " + name
+        }
+    }
+    override fun onResume() {
+        super.onResume()
+        updateUIWithUsername()
+    }
 }
